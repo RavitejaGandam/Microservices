d56fd347f0c828b000b4050df8a7baf3c1ed8d81
raviteja gandam <gandamraviteja@gmail.com>
2024-04-08 16:35:44 +0530

    Some modifications

---
+++ microservices-new/order-service/src/main/java/com/ravi/orderservice/model/Order.java
@@ -0,0 +1,26 @@
+package com.ravi.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+
+
+}
