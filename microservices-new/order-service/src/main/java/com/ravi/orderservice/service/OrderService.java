6c54b8e90db44e5f2031a38203cb7199ad85c9cd
raviteja gandam <gandamraviteja@gmail.com>
2024-04-08 16:52:44 +0530

    Some modifications

---
+++ microservices-new/order-service/src/main/java/com/ravi/orderservice/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.ravi.orderservice.service;
+
+import com.ravi.orderservice.dto.OrderLineItemsDto;
+import com.ravi.orderservice.dto.OrderRequest;
+import com.ravi.orderservice.model.Order;
+import com.ravi.orderservice.model.OrderLineItems;
+import com.ravi.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDto()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
